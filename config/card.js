2391ce09b6b2b48ca7473669f410ecd9be8810a1
Joost <joost.berns@gmail.com>
2022-12-06 20:28:41 +0100

    Added site images to 040460 set

--- config/card.js
+++ config/card.js
@@ -5,7 +5,7 @@ export const cards = cardList.map((card) => {
   if (card.set === '040460') {
     const value = parseInt(card.id.replace(/^xf22-(\d+)x3$/i, '$1'), 10);
 
-    if (isNaN(value) || value > 58) {
+    if (isNaN(value) || value > 82) {
       return {
         ...card,
         image: 'images/back.jpg'
