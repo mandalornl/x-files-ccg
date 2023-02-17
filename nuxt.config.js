21ebb05c9015e10999cd71f43d44351e149ccfd2
Joost <joost.berns@gmail.com>
2023-02-17 21:12:56 +0100

    Added og:image

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,12 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
+      { hid: 'og:title', property: 'og:title', content: 'X-Files CCG' },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      { hid: 'og:image', property: 'og:image', content: 'og-1200x630.png' },
+      { hid: 'og:image:width', property: 'og:image:width', content: 1200 },
+      { hid: 'og:image:height', property: 'og:image:height', content: 630 }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: 'favicon.ico' }
