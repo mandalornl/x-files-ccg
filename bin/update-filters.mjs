67b2740e9206599763b97439c5077c8c1151e235
Joost Berns <joost.berns@softmedia.nl>
2025-05-26 14:45:50 +0200

    Bin script to update filters

---
+++ bin/update-filters.mjs
@@ -0,0 +1,72 @@
+import {
+  readFile,
+  writeFile
+} from 'node:fs/promises';
+import { URL } from 'node:url';
+
+const pool = await Promise.all([
+  'premiere',
+  'ttiot',
+  '101361',
+  '22364',
+  '040460',
+  'promo',
+  'gencon',
+  '100617',
+  'classified',
+  'i-want-to-believe',
+  'twin-peaks',
+  '101357'
+].map(async (name) => {
+  const json = await readFile(new URL(`../config/cards/${name}.json`, import.meta.url), 'utf-8');
+
+  return JSON.parse(json);
+})).then((result) => result.flat());
+
+const filters = Object.entries({
+  activators: 'activators',
+  cost: 'costs',
+  episode: 'episodes',
+  keywords: 'keywords',
+  // rarity: 'rarities',
+  // set: 'sets',
+  tags: 'tags',
+  // type: 'types'
+});
+
+const collator = new Intl.Collator('en', {
+  sensitivity: 'base',
+  numeric: true
+});
+
+for (const [
+  key,
+  name
+] of filters) {
+  const data = pool
+    .map((card) => card[key])
+    .flat()
+    .filter((value, index, self) => !!value && self.indexOf(value) === index)
+    .sort((a, b) => {
+      if (key === 'cost') {
+        const [
+          costA,
+          poolA
+        ] = a.split(' ');
+        const [
+          costB,
+          poolB
+        ] = b.split(' ');
+
+        if (poolA === poolB) {
+          return collator.compare(costA, costB);
+        }
+
+        return collator.compare(poolA, poolB);
+      }
+
+      return collator.compare(a, b);
+    });
+
+  await writeFile(new URL(`../config/cards/${name}.json`, import.meta.url), JSON.stringify(data, null, 2));
+}
